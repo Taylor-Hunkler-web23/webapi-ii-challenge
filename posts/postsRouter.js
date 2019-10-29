d5665c770defea5181368b506bb0b6eeda68ba02
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-29 18:01:17 -0400

    Created PUT route to /api/posts/:id to update

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -8,29 +8,29 @@ const db = require('../data/db.js')
 
 router.get('/', (req, res) => {
     db.find()
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({
-        error: 'The posts information could not be retrieved.',
-      });
-    });
-  });
-  
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: 'The posts information could not be retrieved.',
+            });
+        });
+});
 
 
-  //returns posts with specified id
+
+//returns posts with specified id
 
 router.get('/:id', (req, res) => {
     const id = req.params.id;
     db.findById(id)
         .then(post => {
-            if (post.length){
-            res.status(200).json(post);
-            }else{
-                res.status(404).json({message: "The post with the specified ID does not exist."})
+            if (post.length) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
             }
         })
         .catch(err => {
@@ -39,16 +39,16 @@ router.get('/:id', (req, res) => {
         })
 })
 
-  //returns comment with specified id
+//returns comment with specified id
 
-  router.get('/:id/comments', (req, res) => {
+router.get('/:id/comments', (req, res) => {
     const id = req.params.id;
     db.findCommentById(id)
         .then(comment => {
-            if (comment.length){
-            res.status(200).json(comment);
-            }else{
-                res.status(404).json({message: "The post with the specified ID does not exist."})
+            if (comment.length) {
+                res.status(200).json(comment);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
             }
         })
         .catch(err => {
@@ -68,7 +68,7 @@ router.post('/', (req, res) => {
         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
     } else {
         db.insert(req.body)
-
+            .then(user => db.findById(user.id))
             .then(user => {
                 res.status(201).json(user);
             })
@@ -78,6 +78,60 @@ router.post('/', (req, res) => {
             })
     }
 })
+
+
+
+//post comments
+
+// router.post('/:id/comments', (req, res) => {
+//     const id = req.params.id;
+
+//     const { text } = req.body;
+//     if (!text) {
+//         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+//     } else {
+//         db.insertComment(id,req.body)
+
+//             .then(comment => {
+//                 res.status(201).json(comment);
+//             })
+//             .catch(err => {
+//                 console.log('error', err);
+//                 res.status(500).json({ error: "There was an error while saving the post to the database" })
+//             })
+//     }
+// })
+
+
+
+//put
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const { title, contents } = req.body;
+
+
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+
+        db.update(id, req.body)
+        
+            .then(user => {
+                if (user) {
+                    res.status(200).json(user);
+                } else {
+                    res.status(404).json({ message: "The user with the specified ID does not exist." });
+                }
+
+            })
+            .catch(err => {
+                console.log('error', err);
+                res.status(404).json({ error: "The user could not be removed" })
+            })
+    }
+});
+
+
 
 
 
