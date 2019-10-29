dd4d316a40143d8f386bfba987d966e79097e296
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-29 16:11:31 -0400

    Created POST route to /api/posts to create a new post

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -59,4 +59,26 @@ router.get('/:id', (req, res) => {
 
 
 
+//post
+
+router.post('/', (req, res) => {
+
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        db.insert(req.body)
+
+            .then(user => {
+                res.status(201).json(user);
+            })
+            .catch(err => {
+                console.log('error', err);
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+            })
+    }
+})
+
+
+
 module.exports = router;
