b428647f8fa8c03fb33ab078993b75c870db676c
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-29 15:46:23 -0400

    Created route to /api/posts/:id that returns post with specified id

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -21,4 +21,23 @@ router.get('/', (req, res) => {
   
 
 
+  //returns posts with specified id
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+        .then(post => {
+            if (post.length){
+            res.status(200).json(post);
+            }else{
+                res.status(404).json({message: "The post with the specified ID does not exist."})
+            }
+        })
+        .catch(err => {
+            console.log('error', err);
+            res.status(500).json({ error: "The posts information could not be retrieved." })
+        })
+})
+
+
 module.exports = router;
